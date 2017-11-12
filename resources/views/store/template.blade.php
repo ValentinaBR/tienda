1f50b06e6e236a7409310803225bf776457a6ad8
Valentina Burbano R <lvalentinabr1996@gmail.com>
2017-11-12 00:44:42 -0500

    arreglo de template

--- resources/views/store/template.blade.php
+++ resources/views/store/template.blade.php
@@ -17,5 +17,6 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="{{asset('js/pinterest_grid.js')}}"></script>
 </body>
 </html>
