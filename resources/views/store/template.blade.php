fb5571cce19cc0c64021491ad8a912333627ab39
Valentina Burbano R <lvalentinabr1996@gmail.com>
2017-11-12 00:58:38 -0500

    template final

--- resources/views/store/template.blade.php
+++ resources/views/store/template.blade.php
@@ -18,5 +18,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="{{asset('js/pinterest_grid.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
 </body>
 </html>
